29d8dfe080ba486a66cdeaee1ea26afe4d3eb7c8
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-07 16:50:53 +0400

    test: isolate application context from local MySQL

--- src/test/java/com/cv/mysql/MysqlApplicationTests.java
+++ src/test/java/com/cv/mysql/MysqlApplicationTests.java
@@ -3,11 +3,17 @@ package com.cv.mysql;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:sqldemo;MODE=MySQL;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect"
+})
 class MysqlApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
